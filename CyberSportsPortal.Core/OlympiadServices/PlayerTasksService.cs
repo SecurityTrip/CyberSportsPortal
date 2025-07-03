2ae9273add31908f93be514bf148280113050af8
YuraGolyshev <yura.golyshev2024@bk.ru>
2025-07-03 13:49:20 +0400

    Написан FilterPlayers

--- CyberSportsPortal.Core/OlympiadServices/PlayerTasksService.cs
+++ CyberSportsPortal.Core/OlympiadServices/PlayerTasksService.cs
@@ -1,4 +1,6 @@
-﻿using CyberSportsPortal.Data.Model.Views;
+﻿using CyberSportsPortal.Data.Entities;
+using CyberSportsPortal.Data.Model.Views;
+using System;
 using System.Collections.Generic;
 
 namespace CyberSportsPortal.Core.OlympiadServices;
@@ -7,6 +9,24 @@ public class PlayerTasksService
 {
     public List<PlayerView> FilterPlayers(List<PlayerView> players, string filter)
     {
-        return players;
+        List<PlayerView> playersToShow = new List<PlayerView>();
+        if (players == null || string.IsNullOrEmpty(filter))
+        {
+            return new List<PlayerView>();
+        }
+
+        foreach (PlayerView player in players)
+        {
+            if (player.NickName != null && player.NickName.Contains(filter, StringComparison.OrdinalIgnoreCase))
+            {
+                playersToShow.Add(player);
+                continue;
+            }
+            if (player.CombinedName != null && player.CombinedName.Contains(filter, StringComparison.OrdinalIgnoreCase))
+            {
+                playersToShow.Add(player);
+            }
+        }
+        return playersToShow;
     }
 }
